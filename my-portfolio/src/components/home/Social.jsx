966a0138ae5c2c0800e7105b406598d5e9189acd
JasNjoku <=>
2026-01-05 18:31:38 +0000

    Finish Home Section

---
+++ my-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/jason-njoku-034b97268/" className="home__social-icon" target='_blank'>
+            <i className='uil uil-linkedin'></i>
+        </a>
+
+        <a href="https://github.com/JasNjoku" className="home__social-icon" target='_blank'>
+            <i className='uil uil-github'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
