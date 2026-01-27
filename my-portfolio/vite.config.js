bcf1046c314b85f11564cabdc94aed9fefedbc4c
JasNjoku <=>
2026-01-27 19:00:10 +0000

    Edit config file

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/jason-njoku-portfolio/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-      base: "/jason-njoku-portfolio"
     }),
   ],
 })
